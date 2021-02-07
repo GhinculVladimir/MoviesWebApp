a10c5eb94723c0b06fbf4e013d7c2cfb6687bda0
GhinculVladimir <ghinculvladimir@gmail.com>
2021-02-07 21:57:02 +0200

    chore: Create & init project.

---
+++ src/MoviesWebApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MoviesWebApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
